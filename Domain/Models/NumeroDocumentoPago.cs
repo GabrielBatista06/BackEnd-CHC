b0f0b9cb55f0c99292d31ca7b2c60ff300f67037
Gabriel Batista <gabrielmontilla04@gmail.com>
2024-03-17 15:27:28 -0400

    Modelo NumeroDocumentoPago

---
+++ Domain/Models/NumeroDocumentoPago.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace ComercialHermanosCastro.Domain.Models
+{
+    [Table("NumeroDocumentoPago")]
+
+    public class NumeroDocumentoPago
+    {
+
+        [Key]
+        public int IdNumeroDocumento { get; set; }
+        [Column("ultimo_Numero")]
+        public int UltimoNumero { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+
+    }
+}
