db4fa93ce2f5cec317f3d983c3660b05feeec569
Gabriel Batista <gabrielmontilla04@gmail.com>
2024-08-03 20:49:56 -0400

    DTOs CuentasPendientesAtrasadasDto

---
+++ DTOs/CuentasPendientesAtrasadasDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ComercialHermanosCastro.DTOs
+{
+    public class CuentasPendientesAtrasadasDto
+    {
+        public int? Id { get; set; }
+        public int? IdCliente { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Apodo { get; set; }
+        public decimal? Total { get; set; }
+        public int? DiaPago { get; set; }
+        public int? Cuotas { get; set; }
+        public decimal? ValorCuota { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+        public string? NumeroDocumento { get; set; }
+    }
+}
